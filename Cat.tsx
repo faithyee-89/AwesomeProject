d3a0e0c017d0d328fab6b7888a23c22498a07bf3
yewei <yewei@jingxunsoft.com>
2023-12-06 11:03:21 +0800

    初次提交

---
+++ Cat.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, TextInput, View} from 'react-native';
+
+const Cat = () => {
+  return (
+    <View>
+      <Text>Hello, I am...</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="Name me!"
+      />
+    </View>
+  );
+};
+
+export default Cat;
